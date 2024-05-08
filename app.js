666d8ed3973468b212b4ef720f194a8c04caf54d
zarnigor <zarnigorasatova52@gmail.com>
2024-05-08 11:21:19 +0500

    feature

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const musicContainer = document.querySelector("#music-container");
-    const playBtn = document.querySelector("#play");
-    const prevBtn = document.querySelector("#prev");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
 
 const audio = document.querySelector("#music");
@@ -9,67 +9,75 @@ const progressContainer = document.querySelector("#progress-container");
 const title = document.querySelector("#title");
 const cover = document.querySelector("#cover");
 
-const songs = ["1","2","3"];
+const songs = ["anna-asti%20povelo","2","3"];
 let songIndex = 0;
 
-loadSong (songs[songIndex]);
+loadSong(songs[songIndex]);
 
 function loadSong(song){
     title.innerText = song;
     audio.src = `music/${song}.mp3`;
     cover.src = `images/${song}.jpg`;
 }
+
 playBtn.addEventListener("click", () => {
-  const isPlaying = musicContainer.classList.contains("play");
-  if (isPlaying){
-      pauseSong();
-  } else {
-      playSong();
-  }
+    const isPlaying = musicContainer.classList.contains("play");
+    if (isPlaying){
+        pauseSong();
+    } else {
+        playSong();
+    }
 });
 
 function playSong() {
     musicContainer.classList.add("play");
     playBtn.querySelector("i.fas").classList.remove("fa-play");
-    playBtn.querySelector("i.fas").classList.remove("fa-pause");
-    audio.play()
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
+    audio.play();
 }
-function pauseSong() {
-    musicContainer.classList.add("play");
-    playBtn.querySelector("i.fas").classList.remove("fa-play");
-    playBtn.querySelector("i.fas").classList.remove("fa-pause");
-    audio.pause()
-}
-//change song
 
-prevBtn.addEventListener("click",prevSong);
-prevBtn.addEventListener("click",nextSong);
+function pauseSong() {
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fas").classList.remove("fa-pause");
+    playBtn.querySelector("i.fas").classList.add("fa-play");
+    audio.pause();
+}
+
+prevBtn.addEventListener("click", prevSong);
 
 function prevSong(){
-    songIndex --;
+    songIndex--;
     if (songIndex < 0) {
-        songIndex = song.length - 1;
+        songIndex = songs.length - 1;
     }
     loadSong(songs[songIndex]);
     playSong();
 }
 
-//time song update
+// Next song function
+nextBtn.addEventListener("click", nextSong);
 
-audio.addEventListener("timeupdate",updateProgress);
+function nextSong() {
+    songIndex++;
+    if (songIndex === songs.length) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+audio.addEventListener("timeupdate", updateProgress);
 function updateProgress (e) {
-    const { duration, curreTime} = e.srcElement;
+    const { duration, currentTime } = e.srcElement;
     const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
 }
 
-// Click on progress bar
+progressContainer.addEventListener("click", setProgress);
 
-
-
-
-
-
-
-
-
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
